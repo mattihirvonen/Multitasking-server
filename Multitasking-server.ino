209a8c4b2a0219169d1a141a90e3fe4d0781834a
mattihirvonen <kuukkeli.matti.hirvonen@gmail.com>
2026-02-05 06:21:32 +0200

    Add more informative comments to formal INO file

--- Multitasking-server.ino
+++ Multitasking-server.ino
@@ -1,6 +1,11 @@
 //===========================================================//
 //                                                           //
-//  This is dummy ArduinoIDE required project INO file.      //
+//  This is dummy INO file to satisfy Arduino IDE/CLI        //
+//  tool required project's formal INO file.                 //
+//                                                           //
+//  DO NOT OVER WRITE THIS FILE TO CONTAIN FUNCTIONS         //
+//  - setup()                                                //
+//  - loop()                                                 //
 //                                                           //
 //  All real working sources are in "src" sub directory !!!  //
 //                                                           //
